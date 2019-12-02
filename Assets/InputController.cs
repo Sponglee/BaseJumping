bb8683d9da77d29e5f07f691c63d3513e700e7fa
SolidHinken <solidhinken@gmail.com>
2019-12-02 19:02:58 +0300

    Initial commit

---
+++ Assets/InputController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class InputController : MonoBehaviour
+{
+    //DEBUG
+    public CinemachineVirtualCamera normalCam;
+    public CinemachineVirtualCamera slowCam;
+    public CinemachineVirtualCamera speedCam;
+
+    public CinemachineVirtualCamera startCam;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(2))
+        {
+            startCam.m_Priority = 0;
+            LevelMover.Instance.Moving = true;
+        }
+
+
+        if(Input.GetMouseButton(0))
+        {
+            speedCam.m_Priority = 11;
+            slowCam.m_Priority = 8;
+            normalCam.m_Priority = 9;
+        }
+        else if(Input.GetMouseButton(1))
+        {
+            speedCam.m_Priority = 7;
+            slowCam.m_Priority = 11;
+            normalCam.m_Priority = 8;
+        }
+        else
+        {
+            speedCam.m_Priority = 7;
+            slowCam.m_Priority = 8;
+            normalCam.m_Priority = 10;
+
+        }
+    }
+}
---
+++ Assets/LevelMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMover : Singleton<LevelMover>
+{
+    public float levelSpeed = 1f;
+    public float spawnOffset = 700f;
+    public Transform spawnPoint;
+    public bool Moving = false;
+
+    //DEBUG SPAWN
+    public GameObject segmentPref;
+    public void SpawnSegment()
+    {
+        Instantiate(segmentPref, spawnPoint);
+    }
+
+   
+}
