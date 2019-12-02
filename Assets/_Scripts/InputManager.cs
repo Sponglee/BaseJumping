c78734999a78f9af9f31db2f9f7e01d5dec52086
SolidHinken <solidhinken@gmail.com>
2019-12-02 19:40:42 +0300

    02.12.19
    Movement mockup

---
+++ Assets/_Scripts/InputManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : Singleton<InputManager>
+{
+
+    [SerializeField] private protected Joystick joystick;
+
+
+    public Vector3 charInput;
+
+   
+
+
+    private void Update()
+    {
+         charInput = new Vector3(joystick.Horizontal, 0f, joystick.Vertical);
+
+        //playerPivot.position = transform.position + Quaternion.AngleAxis(-20f, Vector3.up) * charInput * 20f;
+    }
+  
+}
