f95b9cd104ef67a5f5177504941dc626fff0d2b3
SolidHinken <solidhinken@gmail.com>
2019-12-15 20:50:50 +0300

    15.12.19
    Separated Movement from Input

---
+++ Assets/CharacterMover.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+
+    public Transform playerMover;
+    public InputManager _inputManager;
+    private LevelMover _levelMover;
+
+    [Header("MovementControls")]
+    [SerializeField] private float sideSpeed = 1f;
+    [SerializeField] private float xBound = 1f;
+    [SerializeField] private float zBound = 1f;
+    [SerializeField] private float moveResistance = 0.2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _inputManager = GetComponent<InputManager>();
+        _levelMover = LevelMover.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_levelMover.Moving)
+        {
+            playerMover.localPosition = _inputManager.charInput;
+            playerMover.localPosition = new Vector3(Mathf.Clamp(playerMover.localPosition.x, -xBound, xBound),
+                playerMover.localPosition.y,
+                Mathf.Clamp(playerMover.localPosition.z, -zBound, zBound));
+
+            if (Mathf.Abs(playerMover.localPosition.x) > moveResistance || Mathf.Abs(playerMover.localPosition.z) > 0.2f)
+            {
+                //Debug.Log("MOVE");
+                _levelMover.offsetDir = -(sideSpeed * playerMover.localPosition);
+            }
+        }
+    }
+}
--- Assets/_Scripts/InputManager.cs
+++ Assets/_Scripts/InputManager.cs
@@ -4,37 +4,15 @@ using UnityEngine;
 
 public class InputManager : Singleton<InputManager>
 {
-
-    //[SerializeField] private protected Joystick joystick;
-
     public Transform playerMover;
-
-
-
 
     public Vector3 delta;
     public Vector3 charInput;
     public Vector3 touchPos;
 
-
-
-    private LevelMover _levelMover;
-
-    [Header("MovementControls")]
-    [SerializeField] private float sideSpeed = 1f;
-    [SerializeField] private float xBound = 1f;
-    [SerializeField] private float zBound = 1f;
-    [SerializeField] private float moveResistance = 0.2f;
-
-    private void Start()
-    {
-        _levelMover = LevelMover.Instance;
-    }
-
     private void Update()
     {
-        if (_levelMover.Moving)
-        {
+       
             Vector3 mousePos = Input.mousePosition;
             mousePos.z = 10;
 
@@ -42,42 +20,11 @@ public class InputManager : Singleton<InputManager>
             if (Input.GetMouseButtonDown(0))
             {
                 delta = (touchPos - playerMover.position);
-
-
-
-
             }
             else if (Input.GetMouseButton(0))
             {
-                //if (Mathf.Abs(playerMover.localPosition.x) >= 1.3f || Mathf.Abs(playerMover.localPosition.z) >= 1f)
-                //{
-                //    delta = (touchPos - playerMover.position);
-                //}
-
                 charInput = new Vector3(touchPos.x - delta.x, 0f, touchPos.z - delta.z);
-                playerMover.localPosition = charInput;
-
-
-
-                playerMover.localPosition = new Vector3(Mathf.Clamp(playerMover.localPosition.x, -xBound, xBound),
-                    playerMover.localPosition.y,
-                    Mathf.Clamp(playerMover.localPosition.z, -zBound, zBound));
-
-
-                if (Mathf.Abs(playerMover.localPosition.x) > moveResistance || Mathf.Abs(playerMover.localPosition.z) > 0.2f)
-                {
-                    //Debug.Log("MOVE");
-                    _levelMover.offsetDir = -(sideSpeed * playerMover.localPosition);
-                }
             }
-            else if (Input.GetMouseButtonUp(0))
-            {
-                //charInput = Vector3.zero;
-            }
-        }
-       
-
-
     }
 
 }
